8598d4cbee8b7d9e40724dbb7f347af45661670b
Aareen Dakway <aareend@gmail.com>
2025-10-29 18:08:01 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+name = input("What is your name? ")
+age = input("How old are you? ")
+print(f"Welcome {name}, you are {age} years old!")
+
+
---
+++ variables.py
@@ -0,0 +1,38 @@
+# # x = 7
+# # y = x
+# # print( y + 1 , id(y + 1))
+# # print (x, id(x))
+# # print (y, id(y))
+# # fname = "John"
+# # print (fname[0])
+
+# username = "Aareen"
+# password = "mypassword123"
+# print("Enter your username:")
+# username = input().strip()
+# print("Enter your password:")
+# password = input().strip()
+
+# # if username == "Aareen" and password == "mypassword123":
+# #     print("you are authenticated")
+# # else:
+# #     print("Authentication failed")
+
+# otp = "4567"
+# limit = 0
+
+# while limit < 3:
+#     limit = limit + 1
+#     if username == "Aareen" and password == "mypassword123":
+#         otp1 = input("Enter OTP: ")
+#         if otp == otp1:
+#             print("OTP verified")
+#         else:
+#             print("Invalid OTP") 
+
+#     elif username != "Aareen" and password != "mypassword123":
+#         print("Both username and password are incorrect")
+
+age = 69
+print(type((str(age))))
+        
