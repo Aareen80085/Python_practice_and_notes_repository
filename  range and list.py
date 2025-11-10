b2cda226346e18e8523d3dd498afbdf474e56597
Aareen Dakway <aareend@gmail.com>
2025-11-10 13:36:13 +0530

    Add files via upload

---
+++  range and list.py
@@ -0,0 +1,46 @@
+#using range() function
+
+
+
+# range(start,stop,step)
+# range(5)
+# range(0,5)
+# range(0,5,2)
+# print(list(range(5)))
+# print(list(range(0,5)))
+# print(list(range(0,5,2)))
+nums=[1,2,3,4]
+# for x in range(0,5):
+#     print(x)
+
+# nums=range(0,5)
+# for x in nums:
+#     print(x)
+# nums="prasad"
+# for x in reversed(nums):
+#     print(x)
+
+# nums="prasad"
+# [print(x) for x in nums]
+# print(len(nums))
+
+# print(nums[2])
+# nums[2]=True
+# print(nums[2])
+
+# nums.pop(0)
+# print(nums)
+# nums.append(7)
+
+# print(nums)
+# nums.insert(1,9)
+
+# print(nums)
+# nums.remove(4)
+
+# for x in nums:
+#     print(x)
+
+print(nums[::-1])
+print(nums[:3])
+print(nums[1:3:1])
--- operators.py
+++ operators.py
@@ -3,10 +3,47 @@
 
 # print(7)
 
-x = 5
-z = 10
+# x = int("257")
+# z = int("257")
 
-print(x > 5 and x < 10) #False
-print(x > 5 or x < 10) #True
-print(not x >= 5) #False
-print(not x > 5) #True
+# x = print(id(x))
+# z = print(id(z))
+
+
+# a = int("3")
+# b = int("3")
+
+# a = print(id(a))
+# b = print(id(b))
+
+# Logical operators
+
+# print(x > 5 and x < 10) #False
+# print(x > 5 or x < 10) #True
+# print(not x >= 5) #False
+# print(not x > 5) #True
+
+# Membership operators
+
+# print(x is z) #False
+# print(a is b) #True
+
+# print(x == z) #True
+
+# fname = "Aareen"
+# print("Aareen" in fname)
+
+# print("A" in fname)
+# print("r" not in fname)
+
+# Strings functions:-
+
+fname = "sai"
+print(fname)
+
+print(fname[-1])
+
+fname.upper()
+fname.lower()
+fname.title()
+
